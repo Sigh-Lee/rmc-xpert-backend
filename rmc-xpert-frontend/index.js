383af47b59b6dc10401176d411bb96250e67cd68
Sigh-Lee <naskiller94@gmail.com>
2025-07-13 12:21:54 +0200

    index.js

--- rmc-xpert-frontend/index.js
+++ rmc-xpert-frontend/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 const App = () => {
   return (
     <div style={{ backgroundColor: '#121212', color: '#fff', fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>🚀 RMC X-PERT Client Interface</h1>
+      <h1>{"🚀 RMC X-PERT Client Interface"}</h1>
       <p>Connected to Backend: {import.meta.env.VITE_API_URL}</p>
     </div>
   );
